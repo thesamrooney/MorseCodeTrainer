fcb228f3ed411585dac2c93531b41dcae873ddd3
Sam Rooney <the.sam.rooney@gmail.com>
2022-06-17 20:29:40 -0400

    Create audio-frequency timing function generator

--- sound/ToneGenerator.py
+++ sound/ToneGenerator.py
@@ -14,6 +14,22 @@ class ToneGenerator():
         t = np.arange(0, length, 1/self.sample_rate)
         tone = np.int16(np.sin(2*np.pi*freq*t)*32767*vol)
 
+    def make_timing_function(self, timing: str):
+        output = np.array()
+        for c in timing:
+            if c == "-":
+                # high: value of 1
+                for i in np.arange(0, self.time_unit_s, 1/self.sample_rate):
+                    np.append(output, [1])
+            elif c == "_":
+                # low: value of 0
+                for i in np.arange(0, self.time_unit_s, 1/self.sample_rate):
+                    np.append(output, [0])
+            else:
+                # invalid timing character
+                raise ValueError("Invalid timing character " + c + " in: " + timing)
+        return output
+
     def writeWav(self, name, signal):
         wavfile.write(name, self.sample_rate, signal)
 
